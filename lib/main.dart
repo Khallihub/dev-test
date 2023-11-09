9e253739abd9ae8b01e71e0453fb495c5e26ee20
KHALID ABDU <khalid11abdu@gmail.com>
2023-11-09 16:00:13 +0300

    bug

--- lib/main.dart
+++ lib/main.dart
@@ -87,93 +87,99 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             body: Stack(
               children: <Widget>[
+                _pages[_currentIndex],
                 if (!hideUI.value)
                   Positioned(
                     top: 8.0,
                     left: 16.0,
                     child: PopupMenuButton(
-                      icon: const Icon(Icons.menu, size: 30.0, color: Colors.white),
+                      icon: const Icon(Icons.menu,
+                          size: 30.0, color: Colors.white),
                       itemBuilder: (BuildContext context) {
                         return [
                           const PopupMenuItem(
-                            value: 1,
-                            child: Row(
-                              children: [
-                                Icon(Icons.add_box_rounded, color: Color.fromRGBO(99, 129, 199, 1),),
-                                SizedBox(width: 12.0),
-                                Text('Post a Seequl'),
-                              ],
-                            )
-                          ),
+                              value: 1,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.add_box_rounded,
+                                    color: Color.fromRGBO(99, 129, 199, 1),
+                                  ),
+                                  SizedBox(width: 12.0),
+                                  Text('Post a Seequl'),
+                                ],
+                              )),
                           const PopupMenuItem(
-                            value: 2,
-                            child: Row(
-                              children: [
-                                Icon(Icons.favorite, color: Color.fromRGBO(251, 94, 94, 1),),
-                                SizedBox(width: 12.0),
-                                Text('View your likes'),
-                              ],
-                            )
-                          ),
+                              value: 2,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.favorite,
+                                    color: Color.fromRGBO(251, 94, 94, 1),
+                                  ),
+                                  SizedBox(width: 12.0),
+                                  Text('View your likes'),
+                                ],
+                              )),
                           const PopupMenuItem(
-                            value: 3,
-                            child: Row(
-                              children: [
-                                Icon(Icons.circle),
-                                SizedBox(width: 12.0),
-                                Text('Your Seequl posts'),
-                              ],
-                            )
-                          ),
+                              value: 3,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.circle),
+                                  SizedBox(width: 12.0),
+                                  Text('Your Seequl posts'),
+                                ],
+                              )),
                           const PopupMenuItem(
-                            value: 4,
-                            child: Row(
-                              children: [
-                                Icon(Icons.menu_book_outlined, color: Color.fromRGBO(95, 99, 104, 1),),
-                                SizedBox(width: 12.0),
-                                Text('Reference Contribution'),
-                              ],
-                            )
-                          ),
+                              value: 4,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.menu_book_outlined,
+                                    color: Color.fromRGBO(95, 99, 104, 1),
+                                  ),
+                                  SizedBox(width: 12.0),
+                                  Text('Reference Contribution'),
+                                ],
+                              )),
                           const PopupMenuItem(
-                            value: 5,
-                            child: Row(
-                              children: [
-                                Icon(Icons.tag, color: Color.fromRGBO(95, 99, 104, 1),),
-                                SizedBox(width: 12.0),
-                                Text('HashTag Challenges'),
-                              ],
-                            )
-                          ),
+                              value: 5,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.tag,
+                                    color: Color.fromRGBO(95, 99, 104, 1),
+                                  ),
+                                  SizedBox(width: 12.0),
+                                  Text('HashTag Challenges'),
+                                ],
+                              )),
                           const PopupMenuItem(
-                            value: 6,
-                            child: Row(
-                              children: [
-                                Icon(Icons.notifications, color: Color.fromRGBO(42, 81, 24, 1),),
-                                SizedBox(width: 12.0),
-                                Text('Notifications'),
-                              ],
-                            )
-                          ),
+                              value: 6,
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.notifications,
+                                    color: Color.fromRGBO(42, 81, 24, 1),
+                                  ),
+                                  SizedBox(width: 12.0),
+                                  Text('Notifications'),
+                                ],
+                              )),
                           const PopupMenuItem(
-                            value: 7,
-                            child: Row(
-                              children: [
-                                Icon(Icons.info, color:Color.fromRGBO(128, 93, 93, 1)),
-                                SizedBox(width: 12.0),
-                                Text('About SeeQul'),
-                              ],
-                            )
-                          ),
-                          // Add more menu items as needed
+                              value: 7,
+                              child: Row(
+                                children: [
+                                  Icon(Icons.info,
+                                      color: Color.fromRGBO(128, 93, 93, 1)),
+                                  SizedBox(width: 12.0),
+                                  Text('About SeeQul'),
+                                ],
+                              )),
                         ];
-                      },
-                      onSelected: (value) {
-                        // Handle the selected menu item here
                       },
                     ),
                   ),
-                _pages[_currentIndex],
               ],
             ),
             bottomNavigationBar: Visibility(
